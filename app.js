5a88e91f16e2c0f1fcc64113620722ea4bb252b6
amit <amitlaloush@gmail.com>
2021-10-02 13:44:59 +0300

    init npm

---
+++ app.js
@@ -0,0 +1,13 @@
+const e = (...s) => {
+    const result = s.join(' ')
+    console.log(result)
+}
+
+const d = (...s) => {
+    const result = s.join(' ')
+    console.log(result)
+}
+module.exports = {
+    e,
+    d,
+};
